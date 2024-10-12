82637737dd95ceb54c11e5852b1fab31cf9f800b
RichardDxd <rlandaamao@gmail.com>
2024-10-11 22:25:05 -0500

    first commit

---
+++ src/main/java/pe/edu/cibertec/t2_gateway/T2GatewayApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.t2_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/t2_gateway/T2GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.t2_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
